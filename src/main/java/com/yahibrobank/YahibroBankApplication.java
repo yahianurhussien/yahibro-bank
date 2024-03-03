082f19d9d12d4ce0629812e8595d7357fe03fcc2
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-03-02 20:14:42 -0600

    initial commit

---
+++ src/main/java/com/yahibrobank/YahibroBankApplication.java
@@ -0,0 +1,13 @@
+package com.yahibrobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahibroBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YahibroBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yahibrobank/yahibrobank/YahibroBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahibrobank.yahibrobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahibroBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
